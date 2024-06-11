bb15bc9df17100cd7938b8fd07ab483293d42afb
piggy-devil <muupaza@gmail.com>
2024-06-11 09:27:49 +0700

    add header, create item page, refactor homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { auth } from "@/auth";
-import { SignIn } from "@/components/sign-in";
-import { SignOut } from "@/components/sign-out";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { database } from "@/db/database";
-import { bids as bidsSchema, items } from "@/db/schema";
-import { revalidatePath } from "next/cache";
 
 export default async function HomePage() {
   const session = await auth();
@@ -18,27 +12,17 @@ export default async function HomePage() {
   if (!user) return null;
 
   return (
-    <main className="container mx-auto py-12">
-      {session ? <SignOut /> : <SignIn />}
+    <main className="container mx-auto py-12 space-y-8">
+      <h1 className="text-4xl font-bold">Items For Sale</h1>
 
-      {session?.user?.name}
-
-      <form
-        action={async (formData: FormData) => {
-          "use server";
-          await database.insert(items).values({
-            name: formData.get("name") as string,
-            userId: session.user?.id!,
-          });
-          revalidatePath("/");
-        }}
-      >
-        <Input name="name" placeholder="Name your item" />
-        <Button type="submit">Post Item</Button>
-      </form>
-      {allItems.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      <div className="grid grid-cols-4 gap-8">
+        {allItems.map((item) => (
+          <div key={item.id} className="border p-8 rounded-xl">
+            {item.name}
+            starting price: ${item.startingPrice / 100}
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
